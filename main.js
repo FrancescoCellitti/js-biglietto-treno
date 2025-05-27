0bee3188283d61b48b503be5ab5d5c068fe5e932
FrancescoCellitti <francescocellitti@virgilio.it>
2025-05-27 15:31:11 +0200

    Refactor price calculation logic and ensure consistent data type usage

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-const km = Number(prompt("inserire numero di km"))
-const età = Number(prompt("inserire età del passeggero"))
+const km = parseInt(prompt("inserire numero di km"))
+const età = parseInt(prompt("inserire età del passeggero"))
 let prezzo = km * 0.21
 
 if (età < 18) {
-    prezzo = (km * 0.21) * 0.8
-} else if (età >= 65) {
-    prezzo = (km * 0.21) * 0.6
-} else {
-    prezzo = km * 0.21
-
+    prezzo *= 0.8
+} else if (età >= 65){
+    prezzo *= 0.6
 }
+
 const prezzoFinale = prezzo.toFixed(2)
 console.log(prezzoFinale)
 
